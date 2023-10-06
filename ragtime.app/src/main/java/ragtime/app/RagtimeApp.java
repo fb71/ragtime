e073a43b02271ef63d54ca896d78cbc5bc82402e
falko <falko@delli>
2023-10-06 17:09:03 +0200

    [#4]: [App] Browser back + max width (https://github.com/fb71/ragtime/issues/4)

--- ragtime.app/src/main/java/ragtime/app/RagtimeApp.java
+++ ragtime.app/src/main/java/ragtime/app/RagtimeApp.java
@@ -15,12 +15,7 @@ package ragtime.app;
 
 import java.util.concurrent.Callable;
 
-import org.teavm.jso.JSBody;
-import org.teavm.jso.JSObject;
-import org.teavm.jso.JSProperty;
 import org.teavm.jso.browser.Window;
-import org.teavm.jso.json.JSON;
-
 import areca.common.Platform;
 import areca.common.ProgressMonitor;
 import areca.common.Promise;
@@ -28,11 +23,12 @@ import areca.common.base.Consumer.RConsumer;
 import areca.common.log.LogFactory;
 import areca.common.log.LogFactory.Level;
 import areca.common.log.LogFactory.Log;
-import areca.rt.teavm.PopStateEvent;
+import areca.rt.teavm.SimpleBrowserHistoryStrategy;
+import areca.rt.teavm.TeaApp;
 import areca.rt.teavm.TeaPlatform;
 import areca.rt.teavm.ui.UIComponentRenderer;
-import areca.ui.App;
-import areca.ui.Size;
+import areca.ui.component2.UIComposite;
+import areca.ui.layout.MaxWidthLayout;
 import areca.ui.pageflow.Page;
 import areca.ui.pageflow.Pageflow;
 
@@ -41,7 +37,7 @@ import areca.ui.pageflow.Pageflow;
  * @author Falko Bräutigam
  */
 public class RagtimeApp
-        extends App {
+        extends TeaApp {
 
     private static final Log LOG = LogFactory.getLog( RagtimeApp.class );
 
@@ -61,25 +57,22 @@ public class RagtimeApp
         UIComponentRenderer.start();
 
         try {
-            // Browser history
-            Window.current().addEventListener( "popstate", ev -> onBrowserHistoryEvent( ev.cast() ) );
-
-            // UI
             new RagtimeApp().createUI( rootWindow -> {
-                rootWindow.size.defaultsTo( () -> {
-                    var body = Window.current().getDocument().getBody();
-                    var size = Size.of( body.getClientWidth(), body.getClientHeight() );
-                    LOG.debug( "BODY: " + size );
-                    return size;
-                });
-
                 String pathName = Window.current().getLocation().getPathName();
                 LOG.info( "URI path: %s", pathName );
 
-                Pageflow.start( rootWindow )
+                rootWindow.layout.set( MaxWidthLayout.width( 500 ).fillHeight.set( true ) );
+                var pageflowContainer = rootWindow.add( new UIComposite() {{
+                    cssClasses.add( "MaxWidth" );
+                }});
+                rootWindow.layout();
+
+                Pageflow.start( pageflowContainer )
                         .create( new SelfAwarenessPage() )
                         .putContext( new OAIImageLab( OAIImageLab.KEY ), Page.Context.DEFAULT_SCOPE )
                         .open();
+
+                SimpleBrowserHistoryStrategy.start( Pageflow.current() );
             });
         }
         catch (Throwable e) {
@@ -89,44 +82,6 @@ public class RagtimeApp
             throw (Exception)rootCause;
         }
     }
-
-
-    /**
-     *
-     */
-    public static abstract class BrowserHistoryState
-            implements JSObject {
-
-        @JSBody(params = "newState", script = "return {state: newState};")
-        public static native BrowserHistoryState create( String newState );
-
-        @JSProperty
-        public abstract String getState();
-    }
-
-    public static void onPageOpen( String browserHistoryState ) {
-        var state = BrowserHistoryState.create( browserHistoryState );
-        Window.current().getHistory().pushState( state, "", browserHistoryState );
-    }
-
-    public static void closeTopPage() {
-        Window.current().getHistory().back();
-    }
-
-    /**
-     *
-     */
-    private static void onBrowserHistoryEvent( PopStateEvent ev ) {
-        LOG.info( "popstate: %s", JSON.stringify( ev.getState() ) );
-        ev.preventDefault();
-
-        catchAll( () -> {
-            var pageflow = Pageflow.current();
-            pageflow.close( pageflow.pages().first().orElseError() );
-            return null;
-        });
-    }
-
 
     /**
      * Helps to handle exceptions in any code that is not handled
