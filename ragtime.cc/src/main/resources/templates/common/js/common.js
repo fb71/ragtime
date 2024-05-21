6b6ea63b03d321e10fc4b207737db4ab96db5145
falko <falko@delli>
2024-05-21 15:47:11 +0200

    [#50]: [CC] Topics (https://github.com/fb71/ragtime/issues/50)
    Touch: click + long press + scroll

--- ragtime.cc/src/main/resources/templates/common/js/common.js
+++ ragtime.cc/src/main/resources/templates/common/js/common.js
@@ -1,51 +1,87 @@
 /**
  */
 window.addLongPressListener = function( elm, callback) {
-    //window.console.log( "addLongPressListener: " + elm.className );
     let timer;
+    let lastTouch;
+    let t2;
+    let delta;
 
+    // onStart
     let onStart = (ev) => {
-        log( "onStart" );
+        log( "onStart (timer=" + timer + ")" );
+        
+        // prevent cycle on touchmove/start
+        if (timer != null) {
+            return
+        }
+        if (ev.touches) {
+            delta = 0;
+            lastTouch = ev.touches[0];
+            log( "    startTouch=" + lastTouch );
+        }
         ev.stopPropagation();
         ev.preventDefault();
         ev.stopImmediatePropagation(); // no hover, no click, no nothing
         
         timer = setTimeout( () => {
             log( "timeout 500" );
-            timer = null;
-            callback( ev );
+            if (timer) {
+                timer = null;
+                callback( ev );
+            }
         }, 500 );
         log( "timer: " + timer );
     };
     elm.addEventListener( 'touchstart', onStart );
     elm.addEventListener( 'mousedown', onStart );
 
+    // onEnd
     let onEnd = (ev) => {
         log( "onEnd" );
         if (timer) {
-            log( "element.click(): " + ev.target );
-            const event = new MouseEvent( "click", {
+            log( "dispatchEvent(click): " + ev.target );
+            ev.target.dispatchEvent( new MouseEvent( "click", {
                 view: window,
                 bubbles: true,
                 cancelable: true,
-            });
-            ev.target.dispatchEvent( event );
+            }));
         }
         clearTimeout( timer );
+        timer = null;
     };
     elm.addEventListener( 'touchend', onEnd );
     elm.addEventListener( 'mouseup', onEnd );
     
-    // to sensitive on my phone
-//    elm.addEventListener( 'touchmove', () => {
-//        window.console.log( "touchMove..." );        
-//        clearTimeout( longPressTimer );
-//    });
+    // onMove
+    elm.addEventListener( 'touchmove', (ev) => {
+        let currentTouch = ev.changedTouches[0]
+        if (lastTouch) {
+            //log( "    body:" + document.body.scrollTop )
+            //document.body.scrollTop -= (delta * 4)
+
+            let d = currentTouch.clientY - lastTouch.clientY;
+            delta += d;
+            log( "d: " + d + ", delta: " + delta );
+            
+            clearTimeout( t2 );
+            t2 = setTimeout(() => {
+                log( "scrollBy: " + delta );
+                window.scrollBy({
+                    top: -delta,
+                    behavior: "smooth",
+                });
+            }, 10 );
+            
+            if (timer && Math.abs( d ) > 1) {
+                clearTimeout( timer );
+                timer = null;            
+            }
+        }
+        lastTouch = currentTouch        
+    });
 }
 
 
 function log( msg ) {
-    if (true ) {
-        window.console.log( "LongPress: " + msg );
-    }    
+    //window.console.log( "LongPress: " + msg );
 }
